b80bbb2e8f4fc5e29ab9f4f59b9f089ddd2c3ef7
Oghenemarho Orukele <orukele.dev@gmail.com>
2024-11-18 21:13:42 +0100

    worked on split iterator

--- src/strings.rs
+++ src/strings.rs
@@ -44,6 +44,48 @@ pub fn split_by_delimiter_idomatic(input: &str, delimiter: char) -> Vec<String> 
     res
 }
 
+pub struct SplitByDelimiter<'input> {
+    input: &'input str,
+    delimiter: char,
+    current_pos: usize,
+}
+
+impl<'input> SplitByDelimiter<'input> {
+    pub fn new(input: &'input str, delimiter: char) -> Self {
+        Self {
+            input,
+            delimiter,
+            current_pos: 0,
+        }
+    }
+}
+
+impl<'input> Iterator for SplitByDelimiter<'input> {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // if we get to the end, return none
+        if self.current_pos >= self.input.len() {
+            return None;
+        }
+
+        let remaining = &self.input[self.current_pos..];
+        // find the delimiter by index
+        if let Some(idx) = remaining.find(self.delimiter) {
+            let result = &remaining[..idx]; // take from the start of the string up to the index
+                                            // current position = current position + idx + length of the delimiter
+            self.current_pos += idx + self.delimiter.len_utf8();
+
+            Some(result.to_string())
+        } else {
+            // If no delimiter found, return the whole string
+            let result = remaining;
+            self.current_pos = self.input.len(); // necessary to do this so as to avoid infinite loop
+            Some(result.to_string())
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -73,5 +115,38 @@ mod tests {
         let expected: Vec<String> = input.split('&').map(|s| s.to_string()).collect();
 
         assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn split_by_delimiter_iter_1() {
+        let input = "ab,cd,ef";
+        let result = SplitByDelimiter::new(input, ',');
+        let expected: Vec<String> = input.split(',').map(|s| s.to_string()).collect();
+
+        for (i, w) in result.enumerate() {
+            assert_eq!(w, expected[i])
+        }
+    }
+
+    #[test]
+    fn split_by_delimiter_iter_2() {
+        let input = "abc";
+        let result = SplitByDelimiter::new(input, ',');
+        let expected: Vec<String> = input.split(',').map(|s| s.to_string()).collect();
+
+        for (i, w) in result.enumerate() {
+            assert_eq!(w, expected[i])
+        }
+    }
+
+    #[test]
+    fn split_by_delimiter_iter_3() {
+        let input = "abc&efg&hij";
+        let result = SplitByDelimiter::new(input, '&');
+        let expected: Vec<String> = input.split('&').map(|s| s.to_string()).collect();
+
+        for (i, w) in result.enumerate() {
+            assert_eq!(w, expected[i]);
+        }
     }
 }
